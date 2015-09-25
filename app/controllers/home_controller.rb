921f151e117d38ff298f1a866cc62fefcaaaf4d0
Gabriele Rulli <rulligabri.95@gmail.com>
2015-09-25 12:12:48 +0200

    impaginazione con Kaminari gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
-
   def index
-    @articles = Article.available.all.order(date: :desc)
-    @categories = Category.all
-    @pages = Page.all
+    @articles = Article.available.all.order(date: :desc).page(params[:page]).per(2)
+    @categories = Category.all.order(:title)
+    @pages = Page.all.order(:title)
     @options = Option.all
   end
 
