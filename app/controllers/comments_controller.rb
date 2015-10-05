3af120a878d38bf9903ef3dece6c867c72fb9391
Gabriele Rulli <rulligabri.95@gmail.com>
2015-10-05 12:56:27 +0200

    comment destroy method done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,9 +14,21 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    if @comment
+      @comment.destroy
+      respond_to do |format|
+        format.html { redirect_to  article_path(@article), notice: 'Comment was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+  end
+
   private
     def comment_params
-      params.require(:comment).permit(:body, :article_id, :user_id, :current_user, :first_name)
+      params.require(:comment).permit(:body, :user_id, :current_user, :first_name)
     end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,13 +3,4 @@ class Category < ActiveRecord::Base
   has_many :articles, through: :article_categories, dependent: :destroy
 
   validates_presence_of :title, :description
-
-#   has_attached_file :image, styles: {
-#     thumb: '200x200>',
-#     medium: '500x500#',
-#     large: '1000x1000>'
-#   }, :path => "blogails/:class/:attachment/:id/:style/:filename.:extension", :url => ":s3_domain_url"
-#
-# # Validate the attached image is image/jpg, image/png, etc
-#   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
